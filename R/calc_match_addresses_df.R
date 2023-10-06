cb9b6b08fc3bed8ada5780c552da71c9070dd2eb
AdnanS <adnan.shroufi@nhs.net>
2023-10-06 17:40:34 +0100

    function update

--- R/calc_match_addresses_df.R
+++ R/calc_match_addresses_df.R
@@ -48,8 +48,8 @@ calc_match_addresses_df <- function(
         y = lookup_df %>%
           dplyr::mutate(JOIN_ADDRESS = .data[[lookup_address_col]]),
         by = c(primary_postcode_col, "JOIN_ADDRESS"),
-        suffix = c("", "_LOOKUP"),
-      ) %>%
+        suffix = c("", "_LOOKUP")
+        ) %>%
         dplyr::select(ID, ID_LOOKUP) %>%
         dplyr::mutate(
           SCORE = 1,
@@ -63,7 +63,13 @@ calc_match_addresses_df <- function(
           na_matches = "na",
           by = "ID"
         ) %>%
-        tidytext::unnest_tokens(., "TOKEN", primary_address_col, to_lower = FALSE, drop = FALSE) %>%
+        tidytext::unnest_tokens(
+          #tbl = .,
+          output = "TOKEN",
+          input = primary_address_col,
+          to_lower = FALSE,
+          drop = FALSE
+          ) %>%
         dplyr::group_by(ID) %>%
         dplyr::mutate(
           TOKEN_WEIGHT = dplyr::if_else(grepl("[0-9]", TOKEN) == TRUE, 4, 1),
@@ -74,38 +80,55 @@ calc_match_addresses_df <- function(
 
       # Tokenise lookup addresses
       lookup_tokens_df <- lookup_df %>%
-        tidytext::unnest_tokens(., "TOKEN", lookup_address_col, to_lower = FALSE, drop = FALSE) %>%
+        tidytext::unnest_tokens(
+          #tbl = .,
+          output = "TOKEN",
+          input = lookup_address_col,
+          to_lower = FALSE,
+          drop = FALSE
+          ) %>%
+        # Only need 1 instance of token per lookup ID
         dplyr::distinct() %>%
-        dplyr::mutate(TOKEN_WEIGHT = dplyr::if_else(grepl("[0-9]", TOKEN) == TRUE, 4, 1))
+        dplyr::mutate(
+          TOKEN_WEIGHT = dplyr::if_else(grepl("[0-9]", TOKEN) == TRUE, 4, 1)
+          )
 
       # Score remaining matches
       non_exact_match_df = dplyr::inner_join(
         x = non_exact_match_df,
         y = lookup_tokens_df,
-        by = primary_postcode_col,
+        by = c(primary_postcode_col, "TOKEN_WEIGHT"),
         suffix = c("", "_LOOKUP"),
         relationship = "many-to-many"
-      ) %>%
+        ) %>%
+        # Remove unwanted token pairs (with jw score < 0.8)
+        dplyr::filter(
+          # Remove NA tokens
+          !is.na(TOKEN),
+          !is.na(TOKEN_LOOKUP),
+          # Tokens share same first letter
+          substr(TOKEN_LOOKUP, 1, 1) == substr(TOKEN, 1, 1) |
+            # Tokens share same second letter
+            substr(TOKEN_LOOKUP, 2, 2) == substr(TOKEN, 2, 2) |
+            # Tokens share same last letter
+            substr(TOKEN_LOOKUP, nchar(TOKEN_LOOKUP), nchar(TOKEN_LOOKUP)) ==
+            substr(TOKEN, nchar(TOKEN_LOOKUP), nchar(TOKEN_LOOKUP)) |
+            # One token is a substring of the other
+            stringr::str_detect(TOKEN, TOKEN_LOOKUP) == 1 |
+            stringr::str_detect(TOKEN_LOOKUP, TOKEN) == 1
+        ) %>%
+        # Score remaining token pairs
         dplyr::mutate(
           SCORE = dplyr::case_when(
             # Exact matches
             TOKEN == TOKEN_LOOKUP ~ 1,
-            # Generate zero scores
-            substr(TOKEN_LOOKUP, 1, 1) != substr(TOKEN, 1, 1) |
-              # Tokens share same second letter
-              substr(TOKEN_LOOKUP, 2, 2) != substr(TOKEN, 2, 2) |
-              # Tokens share same last letter
-              substr(TOKEN_LOOKUP, nchar(TOKEN_LOOKUP), nchar(TOKEN_LOOKUP)) != substr(TOKEN, nchar(TOKEN_LOOKUP), nchar(TOKEN_LOOKUP)) |
-              # One token is a substring of the other
-              stringr::str_detect(TOKEN, TOKEN_LOOKUP) != 1 |
-              stringr::str_detect(TOKEN_LOOKUP, TOKEN) != 1 ~ 0,
-            T ~ stringdist::stringdist(TOKEN, TOKEN_LOOKUP, method = "jw", p = 0.1)
-          ),
-          # Remove scores of less than 0.8
-          SCORE = dplyr::if_else(SCORE < 0.8, 0, SCORE),
-          # Multiply the score by the token weight
-          SCORE = SCORE * TOKEN_WEIGHT
+            (TOKEN != TOKEN_LOOKUP) & (TOKEN_WEIGHT == 4) ~ 0,
+            TOKEN != TOKEN_LOOKUP ~ stringdist::stringsim(TOKEN, TOKEN_LOOKUP, method = "jw", p = 0.1)
+          )
         ) %>%
+        # Remove tokens with score less than 0.8 then multiple by weight
+        dplyr::filter(SCORE > 0.8) %>%
+        dplyr::mutate(SCORE = SCORE * TOKEN_WEIGHT) %>%
         # Sum scores per ID pair
         dplyr::group_by(ID, ID_LOOKUP, MAX_SCORE) %>%
         dplyr::summarise(SCORE = sum(SCORE, na.rm = TRUE)) %>%
